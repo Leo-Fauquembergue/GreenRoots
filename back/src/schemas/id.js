d1cb4c06883d040df2ccdc4d974065a59504f41c
Leo-Fauquembergue <leo.fau1708@gmail.com>
2025-07-08 17:16:51 +0200

    fix: categorySchema updated & idSchema added

---
+++ back/src/schemas/id.js
@@ -0,0 +1,5 @@
+// --- Schéma Générique ---
+// Pour valider les IDs numériques dans les paramètres d'URL (ex: /orders/1)
+export const idSchema = z.object({
+  id: z.coerce.number().int().positive("L'ID doit être un entier positif."),
+});
