ae99e1c9517e850b8b00e4f12b9935573d2df4b0
Leo-Fauquembergue <leo.fau1708@gmail.com>
2025-07-08 18:20:56 +0200

    error management system added

---
+++ back/src/errors/http-error.js
@@ -0,0 +1,7 @@
+export class HttpError extends Error {
+  constructor(statusCode, message) {
+    super(message); // Appelle le constructeur de la classe `Error` parente
+    this.statusCode = statusCode; // Ajoute la propriété `statusCode`
+    this.name = "HttpError"; // Donne un nom spécifique à cette classe d'erreur
+  }
+}
