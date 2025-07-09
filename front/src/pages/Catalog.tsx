016446b845be47a5f5e1325e7a3d92cbdfdf06be
Theorichardo <theori94@gmail.com>
2025-07-09 15:07:10 +0200

    last modif before fetching

--- front/src/pages/Catalog.tsx
+++ front/src/pages/Catalog.tsx
@@ -1,7 +1,7 @@
 export default function Catalog() {
-  return (
-    <div>
-      <h1>Catalogue</h1>
-    </div>
-  );
+	return (
+		<div>
+			<h1>Catalogue</h1>
+		</div>
+	);
 }
--- front/src/pages/Home.tsx
+++ front/src/pages/Home.tsx
@@ -1,46 +1,59 @@
 import "../style/home.scss";
+import backgroundImage from "../assets/background-tree.jpg";
 
 export default function Home() {
-  return (
-    <div className="home-container">
-      {/* Section principale */}
-      <section className="hero">
-        <div className="hero-text">
-          <h1>
-            Plantez un arbre !<br />
-            Respirez demain !
-          </h1>
-          <p>
-            Chez Greenroots, nous croyons qu'un petit geste peut avoir un grand impact. Planter un arbre, virgule, c'est bien plus qu'un acte symbolique. C'est lutter contre le réchauffement climatique en capturant le CO2, c'est restaurer la biodiversité en créant des habitats pour la faune, c'est préserver les sols et les ressources en eau, c'est soutenir les communautés locales grâce à des projets de reforestation durable.
-          </p>
-          <button>En savoir plus</button>
-        </div>
-      </section>
+	return (
+		<div className="home-container">
+			{/* Section principale */}
+			<section
+				className="hero"
+				style={{ backgroundImage: `url(${backgroundImage})` }}
+			>
+				<div className="hero-text">
+					<h1>
+						Plantez un arbre !<br />
+						Respirez demain !
+					</h1>
+					<p>
+						Chez Greenroots, nous croyons qu'un petit geste peut avoir un grand
+						impact. Planter un arbre, c'est bien plus qu'un acte symbolique.
+						C'est lutter contre le réchauffement climatique en capturant le CO2,
+						c'est restaurer la biodiversité en créant des habitats pour la
+						faune, c'est préserver les sols et les ressources en eau, c'est
+						soutenir les communautés locales grâce à des projets de
+						reforestation durable.
+					</p>
+					<button>En savoir plus</button>
+				</div>
+			</section>
 
-      {/* Section cartes */}
-      <section className="cards-section">
-        <div className="card">
-          <img src="/path1.jpg" alt="Arbre nature" />
-          <h3>Accès à la nature</h3>
-          <p>
-            Avec GreenRoots, accédez à une base de données complète d’arbres à planter, avec tous les détails nécessaires.
-          </p>
-        </div>
-        <div className="card">
-          <img src="/path2.jpg" alt="Arbre tropical" />
-          <h3>Essences rares</h3>
-          <p>
-            Découvrez des essences rares, locales ou exotiques, adaptées à votre région et à vos envies de reforestation.
-          </p>
-        </div>
-        <div className="card">
-          <img src="/path3.jpg" alt="Arbre urbain" />
-          <h3>Projets urbains</h3>
-          <p>
-            Impliquez-vous dans des projets urbains ou ruraux pour contribuer à une planète plus verte dès aujourd’hui.
-          </p>
-        </div>
-      </section>
-    </div>
-  );
+			{/* Section cartes */}
+			<section className="cards-section">
+				<div className="card">
+					<img src="/path1.jpg" alt="Arbre nature" />
+					<h3>Accès à la nature</h3>
+					<p>
+						Avec GreenRoots, accédez à une base de données complète d’arbres à
+						planter, avec tous les détails nécessaires.
+					</p>
+				</div>
+				<div className="card">
+					<img src="/path2.jpg" alt="Arbre tropical" />
+					<h3>Essences rares</h3>
+					<p>
+						Découvrez des essences rares, locales ou exotiques, adaptées à votre
+						région et à vos envies de reforestation.
+					</p>
+				</div>
+				<div className="card">
+					<img src="/path3.jpg" alt="Arbre urbain" />
+					<h3>Projets urbains</h3>
+					<p>
+						Impliquez-vous dans des projets urbains ou ruraux pour contribuer à
+						une planète plus verte dès aujourd’hui.
+					</p>
+				</div>
+			</section>
+		</div>
+	);
 }
