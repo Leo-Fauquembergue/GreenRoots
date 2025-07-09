4ccc8895565be98b16b18652efd4fcffef153f41
CanapAlsan <alsan-canap@hotmail.fr>
2025-07-09 11:10:57 +0200

    router created for the feature

---
+++ back/src/routers/regionRouter.js
@@ -0,0 +1,13 @@
+import { Router } from "express";
+import * as regionController from "../controllers/regionController.js"
+
+export const regionRouter = Router();
+
+regionRouter.get("/", regionController.getAllRegions);
+regionRouter.get("/:id", regionController.getOneRegion);
+regionRouter.post("/", regionController.createRegion);
+regionRouter.patch("/:id", regionController.updateRegion);
+regionRouter.delete("/:id", regionController.deleteRegion);
+
+// 🌱 Arbres associés à une région
+regionRouter.get("/:id/catalog-trees", regionController.getTreesByRegion);
