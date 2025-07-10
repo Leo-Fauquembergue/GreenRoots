551128114203f9799cfa652b15a1105f74880daf
CanapAlsan <alsan-canap@hotmail.fr>
2025-07-10 18:09:46 +0200

    V1 de la page panier, création du composent, suppression de l'ancien composant Cart, moise en place du Scss, ajout de donées fictive via l'interfaceI du composent pour le test.

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -3,10 +3,10 @@ import Home from "./pages/Home";
 import Catalog from "./pages/Catalog";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
-import Cart from "./pages/Cart";
 import Profile from "./pages/Profile";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import PanierPage from "./pages/PanierPage";
 
 function App() {
 	return (
@@ -18,7 +18,7 @@ function App() {
 					<Route path="/catalog" element={<Catalog />} />
 					<Route path="/login" element={<Login />} />
 					<Route path="/register" element={<Register />} />
-					<Route path="/cart" element={<Cart />} />
+					<Route path="/PanierPage" element={<PanierPage />} />
 					<Route path="/profile" element={<Profile />} />
 				</Routes>
 			</main>
